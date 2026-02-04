2068aff4b2a375206b3827eabf4094e96da3a847
9663 <mohankumar.murthy@gmail.com>
2026-02-04 12:09:34 +0530

    frontend and backend files

---
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>All Rights Reserved</span>
+        </footer>
+
+
+    </div>
+  )
+}
+
+export default FooterComponent
